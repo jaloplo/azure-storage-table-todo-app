e438d4cfe0effe3f9922ea1ce3f5ffadd8e5998e
Jaime López <jaime.lopez.lopez@live.com>
2020-02-13 20:31:22 +0100

    feat(client): azure storage table as the data source

--- static/app.js
+++ static/app.js
@@ -8,10 +8,10 @@ let TodoTitle = Vue.extend({
 });
 
 let TodoDone = Vue.extend({
-    props: ['id'],
+    props: ['id', 'row', 'done'],
     template: '<div class="todos__done"> \
-                    <input type="checkbox" :id="\'check\' + id" checked/> \
-                    <label :for="\'check\' + id"></label> \
+                    <input type="checkbox" :id="\'check\' + id + row" v-model="done" /> \
+                    <label :for="\'check\' + id + row" @click="$emit(\'done\', id, row)"></label> \
                 </div>'
 });
 
@@ -21,7 +21,7 @@ let TodoItem = Vue.extend({
         TodoTitle,
         TodoDone,
     },
-    template: '<li class="todos__item"><TodoDone :id="todo.id" /><TodoTitle :title="todo.title" /></li>'
+    template: '<li class="todos__item"><TodoDone :id="todo.PartitionKey" :row="todo.RowKey" :done="todo.done" @done="(key, row) => $emit(\'done\', key, row)" /><TodoTitle :title="todo.title" /></li>'
 });
 
 let TodoList = Vue.extend({
@@ -29,7 +29,7 @@ let TodoList = Vue.extend({
     components: {
         TodoItem
     },
-    template: '<ul class="todos"><TodoItem v-for="item in todos" :todo="item" /></ul>'
+    template: '<ul class="todos"><TodoItem v-for="item in todos" :todo="item" @done="(key, row) => $emit(\'done\', key, row)"/></ul>'
 });
 
 
@@ -38,10 +38,18 @@ let TodoList = Vue.extend({
 */
 
 let IntroText = Vue.extend({
-    template: '<input class="input intro__text" type="text" placeholder="Add a task" v-model="text" />',
+    template: '<input class="input intro__text" type="text" placeholder="Add a task" v-model="text" @keyup="sendTodo" />',
     data: () => {
         return {
             text: '',
+        }
+    },
+    methods: {
+        sendTodo(e) {
+            if (e.keyCode === 13 && this.text !== '') {
+                this.$emit('add', this.text);
+                this.text = '';
+            }
         }
     }
 });
@@ -50,14 +58,14 @@ let IntroContent = Vue.extend({
     components: {
         IntroText
     },
-    template: '<div class="intro__content"><IntroText /></div>'
+    template: '<div class="intro__content"><IntroText @add="(text) => { $emit(\'add\', text); }" /></div>'
 });
 
 let Intro = Vue.extend({
     components: {
         IntroContent,
     },
-    template: '<div class="intro"><IntroContent /></div>'
+    template: '<div class="intro"><IntroContent @add="(text) => { $emit(\'add\', text); }"/></div>'
 });
 
 
@@ -89,21 +97,132 @@ let AppHeader = Vue.extend({
 });
 
 let AppContent = Vue.extend({
-    props: ['todos'],
     components: {
         TodoList,
         Intro
     },
-    template: '<div class="app__content"><TodoList :todos="todos"/><Intro /></div>'
+    data: () => {
+        return {
+            todos: []
+        };
+    },
+    template: '<div class="app__content"><TodoList :todos="todos" @done="doneTodo"/><Intro @add="addTodo"/></div>',
+    methods: {
+        addTodo(title) {
+            let service_url = 'https://storetodoapp001.table.core.windows.net/';
+            let table_name = 'todos';
+            let sas = '?sv=2019-02-02&ss=t&srt=sco&sp=rwlacu&se=2020-02-29T06:06:35Z&st=2020-02-11T22:06:35Z&spr=https&sig=XYRJzKv1WM18RSWb2XhORFIz6e5JNQsBdzlw%2FB1mttw%3D';
+
+            let url = `${service_url}${table_name}${sas}`;
+
+            superagent.post(url)
+                .set('Accept', 'application/json')
+                .set('Content-Type', 'application/json')
+                .send({
+                    PartitionKey: 'personal',
+                    RowKey: title,
+                    title: title,
+                    done: false
+                })
+                .then(res => {
+                    superagent.get(url)
+                        .set('Accept', 'application/json')
+                        .then(res => {
+                            let response = res.text;
+                            this.todos = JSON.parse(response).value.map(x => {
+                                return {
+                                    PartitionKey: x.PartitionKey,
+                                    RowKey: x.RowKey,
+                                    title: x.title,
+                                    done: x.done === true
+                                };
+                            });
+                        })
+                        .catch(err => {
+                            console.log('>>> ERROR');
+                        });
+                })
+                .catch(err => {
+                    console.log('>>> ERROR');
+                    console.log(err);
+                });
+        },
+
+        doneTodo(key, row) {
+            let service_url = 'https://storetodoapp001.table.core.windows.net/';
+            let table_name = 'todos';
+            let sas = '?sv=2019-02-02&ss=t&srt=sco&sp=rwlacu&se=2020-02-29T06:06:35Z&st=2020-02-11T22:06:35Z&spr=https&sig=XYRJzKv1WM18RSWb2XhORFIz6e5JNQsBdzlw%2FB1mttw%3D';
+
+            let url = `${service_url}${table_name}(PartitionKey='${key}',RowKey='${row}')${sas}`;
+
+            superagent.put(url)
+                .set('Accept', 'application/json')
+                .set('Content-Type', 'application/json')
+                .send({
+                    PartitionKey: key,
+                    RowKey: row,
+                    title: this.todos.filter(x => x.PartitionKey === key && x.RowKey === row)[0].title,
+                    done: !this.todos.filter(x => x.PartitionKey === key && x.RowKey === row)[0].done
+                })
+                .then(res => {
+                    url = `${service_url}${table_name}${sas}`;
+                    superagent.get(url)
+                        .set('Accept', 'application/json')
+                        .then(res => {
+                            let response = res.text;
+                            this.todos = JSON.parse(response).value.map(x => {
+                                return {
+                                    PartitionKey: x.PartitionKey,
+                                    RowKey: x.RowKey,
+                                    title: x.title,
+                                    done: x.done === true
+                                };
+                            });
+                        })
+                        .catch(err => {
+                            console.log('>>> ERROR');
+                            console.log(err);
+                        });
+                })
+                .catch(err => {
+                    console.log('>>> ERROR');
+                    console.log(err);
+                });
+        }
+    },
+    beforeCreate() {
+        let service_url = 'https://storetodoapp001.table.core.windows.net/';
+        let table_name = 'todos';
+        let sas = '?sv=2019-02-02&ss=t&srt=sco&sp=rwlacu&se=2020-02-29T06:06:35Z&st=2020-02-11T22:06:35Z&spr=https&sig=XYRJzKv1WM18RSWb2XhORFIz6e5JNQsBdzlw%2FB1mttw%3D';
+
+        let url = `${service_url}${table_name}${sas}`;
+
+        superagent.get(url)
+            .set('Accept', 'application/json')
+            .then(res => {
+                let response = res.text;
+                this.todos = JSON.parse(response).value.map(x => {
+                    return {
+                        PartitionKey: x.PartitionKey,
+                        RowKey: x.RowKey,
+                        title: x.title,
+                        done: x.done === true
+                    };
+                });
+            })
+            .catch(err => {
+                console.log('>>> ERROR');
+            });
+    }
 });
 
 let App = Vue.extend({
-    props: ['todos', 'title'],
+    props: ['title'],
     components: {
         AppHeader,
         AppContent
     },
-    template: '<div class="app"><AppHeader :title="title" /><AppContent :todos="todos" /></div>'
+    template: '<div class="app"><AppHeader :title="title" /><AppContent /></div>'
 });
 
 new Vue({
@@ -112,13 +231,9 @@ new Vue({
         App
     },
     data: {
-        title: 'Azure Todo App',
-        todos: [
-            { id: 1, title: 'Lorem ipsum A' },
-            { id: 2, title: 'Lorem ipsum B' }
-        ]
+        title: 'Azure Todo App'
     },
-    template: '<App :title="title" :todos="todos" />'
+    template: '<App :title="title"/>',
 });
 
 Vue.config.devtools = true;
